fa07e1e70b76fe7118b988e3fbad9ffc96a10fe6
LLORENS BOIXADER <a.llorensboixader@edu.gva.es>
2024-11-07 12:00:37 +0100

    Commit dos

--- login.php
+++ login.php
@@ -3,4 +3,7 @@
 $user=$_POST["user"];
 $pwd=$_POST["password"];
 
-echo "El teu usuari és " .$user. " i la teu contrasenya és " .$pwd;
+
+echo "<table border='1'><tr>";
+echo "<td>Usuari</td><td>" .$user. "</td></tr>";
+echo "<tr><td>Contrasenya</td> <td>".$pwd."</td></tr></table>";
